2e417018161a79767d3ec6827d9fbd483203c949
Rosario Nici <rosarionici95@gmail.com>
2022-08-24 22:44:59 +0200

    change  file.js and css

--- file.js
+++ file.js
@@ -2,6 +2,7 @@
 const messageHTMLName = document.querySelector(".message-js-one");
 const messageHTMLKm = document.querySelector(".message-js-two");
 const messageHTMLAge = document.querySelector(".message-js-three")
+const messageHTMLNameTiket = document.querySelector(".nome-pass")
 // Nome passeggero
 const namePass = ("Nome Cognome");
 messageHTMLName.innerHTML = namePass;
@@ -24,25 +25,36 @@ function myFunction() {
     const priceKm = 0.21
     let distanceKM = document.getElementById("distance").value;
     console.log(distanceKM)
-    let priceTiket = 0.21 * (Number(distanceKM))
+    let priceTiket = 0.21 * (Number(distanceKM));
     // calcolo sconto
-    var agePassegerCalc = document.getElementById("inputAge").value
-    console.log(agePassegerCalc)
+    var agePassegerMin = document.getElementById("minorenne").value;
+    console.log(agePassegerMin)
+    let discount = (priceTiket / 100) * 20;
+    let priceTiketDiscount = priceTiket - discount;
+    priceTiket = priceTiketDiscount.toFixed(2);
+    var agePassegerOve = document.getElementById("over-65").value;
+    console.log(agePassegerOve)
 
-    if (agePassegerCalc = "minorenne") {
-        let discount = (priceTiket / 100) * 20;
-        let priceTiketDiscount = priceTiket - discount
-        priceTiket = priceTiketDiscount.toFixed(2)
-        // } else if (agePassegerCalc = document.getElementById("o").value) {
-        //     let OverDiscount = (priceTiket / 100) * 40;
-        //     let priceTiketDiscountOv = priceTiket - OverDiscount
-        //     priceTiket = priceTiketDiscountOv.toFixed(2);
 
-        // }
+    if (agePassegerMin === agePassegerMin) {
 
-        console.log(priceTiket)
+
+    } else if (agePassegerCalc = document.getElementById("over-65").value) {
+        let OverDiscount = (priceTiket / 100) * 40;
+        let priceTiketDiscountOv = priceTiket - OverDiscount;
+        priceTiket = priceTiketDiscountOv.toFixed(2);
+
     }
-
+    // compilazione biglietto
+    var nameTiket = document.getElementById("name")
+    const nomePasseggero = nameTiket
+    messageHTMLNameTiket.innerHTML = nomePasseggero
+    console.log(priceTiket)
+}
+function myFunctionAnnul() {
 
 }
+
+
+
 
